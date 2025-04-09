025a1a4feaa5baa35c51573f7d4e3579189e6e09
juned012 <junedansari9172@gmai.com>
2025-04-09 15:04:25 +0530

    Makes a responsive to containers

--- src/App.jsx
+++ src/App.jsx
@@ -71,8 +71,8 @@ const App = () => {
         <h1 className="text-white text-3xl font-bold text-center py-8">
           🎓 Student Management
         </h1>
-        <div className="flex gap-10">
-          <div className="flex-1/2 bg-gray-200 rounded-md px-10 py-2 h-[420px]">
+        <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
+          <div className="w-full lg:w-1/2 bg-gray-200 rounded-md px-5 md:px-10 py-2 h-auto md:h-[420px] mb-6 lg:mb-0">
             <h1 className="text-green-900 text-3xl font-bold text-center py-8 flex justify-center items-center gap-2">
               <FaUserPlus /> Add Student
             </h1>
@@ -132,7 +132,7 @@ const App = () => {
             </form>
           </div>
 
-          <div className="flex-2/3 bg-gray-200 rounded-md px-10 py-2 h-[420px] overflow-y-auto">
+          <div className="w-full lg:w-1/2 bg-gray-200 rounded-md px-5 md:px-10 py-2 h-auto md:h-[420px] overflow-y-auto">
             <h1 className="text-green-900 text-3xl font-bold text-center py-8 flex justify-center items-center gap-2">
               <FaListAlt /> Student List
             </h1>
