2e825712a9c6f3126a0de57987141ff0c1a0e40d
juned012 <junedansari9172@gmai.com>
2025-04-09 11:07:50 +0530

    Added input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="bg-slate-900 min-h-screen flex justify-center items-center">
+      <div className="w-[500px] bg-gray-100">
+        <h1 className="text-yellow-700 text-2xl font-semibold text-center">
+          🎓 Student Management
+        </h1>
+        <form onSubmit={handleSubmit} className="form">
+          <input type="text" placeholder="Student Name" required />
+          <input type="number" placeholder="Age" required />
+          <input type="text" placeholder="City" required />
+          <button type="submit">Add Student</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
